133d67384ad7b6a993cde3ecf5bb2cd4a4e888e4
Rich Lane <rlane@club.cc.cmu.edu>
2011-02-28 23:26:10 -0800

    use line buffer if available

--- lib/rlui/completion.rb
+++ lib/rlui/completion.rb
@@ -3,7 +3,17 @@ module Completion
   Completor = lambda do |word|
     Readline.completion_append_character = nil
     return unless word
-    candidates = child_candidates(word) + cmd_candidates(word)
+
+    candidates = if Readline.respond_to? :line_buffer
+      if Readline.line_buffer[' ']
+        child_candidates(word)
+      else
+        cmd_candidates(word)
+      end
+    else
+      child_candidates(word) + cmd_candidates(word)
+    end
+        
     if candidates.length == 1 && candidates[0][-1] != '/'
       Readline.completion_append_character = ' '
     end
