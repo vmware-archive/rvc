b5d771a63dce0850baa2b804d1898155ca84c36d
Rich Lane <rlane@vmware.com>
2011-03-02 13:45:29 -0800

    look for a locally installed VMRC

--- lib/rvc/modules/vmrc.rb
+++ lib/rvc/modules/vmrc.rb
@@ -1,6 +1,17 @@
 include RVC::Util
 
-VMRC = ENV['VMRC'] || search_path('vmrc')
+# TODO windows tmp folder
+def _local_vmrc_dir ver
+  File.join("/tmp", "vmrc-#{Process::UID.eid}-#{ver}")
+end
+
+def _find_local_vmrc
+  ver = $vim.serviceInstance.content.about.version
+  path = File.join(_local_vmrc_dir(ver), 'plugins', 'vmware-vmrc')
+  File.exists?(path) && path
+end
+
+VMRC = ENV['VMRC'] || _find_local_vmrc || search_path('vmrc')
 
 def view *paths
   err "VMRC not found" unless VMRC
@@ -12,7 +23,7 @@ def view *paths
       ENV['https_proxy'] = ENV['HTTPS_PROXY'] = ''
       $stderr.reopen("#{ENV['HOME']||'.'}/.rvc-vmrc.log", "w")
       Process.setpgrp
-      exec VMRC, '-M', moref, '-h', $opts[:host], '-u', $opts[:user], '-p', $opts[:password], '--disable-ssl-checking'
+      exec VMRC, '-M', moref, '-h', $opts[:host], '-u', $opts[:user], '-p', $opts[:password]
     end
   end
 end
