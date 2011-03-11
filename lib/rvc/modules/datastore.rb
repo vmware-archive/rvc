d2aef2949e2e9ebe0be64b1ba39688bc90d93e26
Rich Lane <rlane@vmware.com>
2011-03-11 12:00:53 -0800

    add datastore.upload

--- lib/rvc/modules/datastore.rb
+++ lib/rvc/modules/datastore.rb
@@ -10,3 +10,19 @@ def download args
   err "not a datastore file" unless file.is_a? RVC::FakeDatastoreFile
   file.datastore.download file.path, local_path
 end
+
+opts :upload do
+  summary "Upload a file to a datastore"
+  usage "local-path datastore-path"
+end
+
+def upload args
+  local_path = args[0] or err "local path required"
+  datastore_path = args[1] or err "datastore path required"
+  datastore_dir_path = File.dirname datastore_path
+  dir = lookup(datastore_dir_path)
+  err "datastore directory does not exist" unless dir.is_a? RVC::FakeDatastoreFolder
+  err "local file does not exist" unless File.exists? local_path
+  real_datastore_path = "#{dir.path}/#{File.basename(datastore_path)}"
+  dir.datastore.upload real_datastore_path, local_path
+end
