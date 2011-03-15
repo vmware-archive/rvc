661980f79034ce8c0daa8c3a6c515c1a9b325d89
Rich Lane <rlane@vmware.com>
2011-03-15 11:00:21 -0700

    add hosts fake folder to cluster

--- lib/rvc/extensions/ClusterComputeResource.rb
+++ lib/rvc/extensions/ClusterComputeResource.rb
@@ -6,4 +6,17 @@ class RbVmomi::VIM::ClusterComputeResource
   def ls_text r
     " (cluster): cpu #{r['summary.effectiveCpu']/1000} GHz, memory #{r['summary.effectiveMemory']/1000} GB"
   end
+
+  # TODO add datastore, network
+  def children
+    hosts, resourcePool = collect *%w(host resourcePool)
+    {
+      'hosts' => RVC::FakeFolder.new(self, :rvc_host_children),
+      'resourcePool' => resourcePool,
+    }
+  end
+
+  def rvc_host_children
+    RVC::Util.collect_children self, :host
+  end
 end
