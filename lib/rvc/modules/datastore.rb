72a3ee0d235921e8576fddd4774ba1dec838955d
Rich Lane <rlane@club.cc.cmu.edu>
2011-03-17 21:17:18 -0700

    fix FakeDatastoreFolder constant use

--- lib/rvc/modules/datastore.rb
+++ lib/rvc/modules/datastore.rb
@@ -6,7 +6,7 @@ end
 
 def download datastore_path, local_path
   file = lookup(datastore_path)
-  err "not a datastore file" unless file.is_a? RVC::FakeDatastoreFile
+  err "not a datastore file" unless file.is_a? RbVmomi::VIM::Datastore::FakeDatastoreFile
   file.datastore.download file.path, local_path
 end
 
@@ -19,7 +19,7 @@ end
 def upload local_path, datastore_path
   datastore_dir_path = File.dirname datastore_path
   dir = lookup(datastore_dir_path)
-  err "datastore directory does not exist" unless dir.is_a? RVC::FakeDatastoreFolder
+  err "datastore directory does not exist" unless dir.is_a? RbVmomi::VIM::Datastore::FakeDatastoreFolder
   err "local file does not exist" unless File.exists? local_path
   real_datastore_path = "#{dir.path}/#{File.basename(datastore_path)}"
   dir.datastore.upload real_datastore_path, local_path
