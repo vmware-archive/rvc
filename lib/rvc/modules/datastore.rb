48b8da29480103c8eb18c7b1adcb63b5600ad715
Rich Lane <rlane@club.cc.cmu.edu>
2011-03-17 21:17:37 -0700

    add datastore.mkdir

--- lib/rvc/modules/datastore.rb
+++ lib/rvc/modules/datastore.rb
@@ -24,3 +24,20 @@ def upload local_path, datastore_path
   real_datastore_path = "#{dir.path}/#{File.basename(datastore_path)}"
   dir.datastore.upload real_datastore_path, local_path
 end
+
+opts :mkdir do
+  summary "Create a directory on a datastore"
+  arg 'path', "Directory to create on the datastore"
+end
+
+def mkdir datastore_path
+  datastore_dir_path = File.dirname datastore_path
+  dir = lookup(datastore_dir_path)
+  err "datastore directory does not exist" unless dir.is_a? RbVmomi::VIM::Datastore::FakeDatastoreFolder
+  ds = dir.datastore
+  dc = ds.path.find { |o,x| o.is_a? RbVmomi::VIM::Datacenter }[0]
+  name = "#{dir.datastore_path}/#{File.basename(datastore_path)}"
+  $vim.serviceContent.fileManager.MakeDirectory :name => name,
+                                                :datacenter => dc,
+                                                :createParentDirectories => false
+end
