a77802e07b2b914f915f162f2e11df5b79c19ef8
Rich Lane <rlane@club.cc.cmu.edu>
2011-04-24 19:15:20 -0700

    add filesystem session store

---
+++ lib/rvc/filesystem_session.rb
@@ -0,0 +1,60 @@
+module RVC
+
+# XXX validate session name / mark keys
+# XXX permissions
+class FilesystemSession
+  def initialize name
+    @dir = File.join(Dir.tmpdir, "rvc-sessions-#{Process.uid}", name)
+    FileUtils.mkdir_p @dir
+    FileUtils.mkdir_p mark_dir
+    @priv = {}
+  end
+
+  def marks
+    Dir.entries(mark_dir).reject { |x| x == '.' || x == '..' } + @priv.keys
+  end
+
+  def get_mark key
+    if is_private_mark? key
+      @priv[key]
+    else
+      return nil unless File.exists? mark_fn(key)
+      File.readlines(mark_fn(key)).
+        map { |path| RVC::Util.lookup(path.chomp) }.
+        inject([], &:+)
+    end
+  end
+
+  def set_mark key, objs
+    if is_private_mark? key
+      if objs == nil
+        @priv.delete key
+      else
+        @priv[key] = objs
+      end
+    else
+      if objs == nil
+        File.unlink mark_fn(key)
+      else
+        File.open(mark_fn(key), 'w') do |io|
+          objs.each { |obj| io.puts obj.rvc_path_str }
+        end
+      end
+    end
+  end
+
+  private
+
+  def is_private_mark? key
+    return key == '' ||
+           key == '~' ||
+           key == '@' ||
+           key =~ /^\d+$/
+  end
+
+  def mark_dir; File.join(@dir, 'marks') end
+  def mark_fn(key); File.join(mark_dir, key) end
+end
+
+end
+
