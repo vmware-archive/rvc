1a4ac1e6e9e17132f1f73724b442717559a7b0d5
Rich Lane <rlane@vmware.com>
2011-05-26 14:15:37 -0700

    add role.{add,remove}_privilege

--- lib/rvc/modules/role.rb
+++ lib/rvc/modules/role.rb
@@ -67,3 +67,50 @@ def delete name, opts
   err "no such role #{role_name.inspect}" unless role
   cur_auth_mgr.RemoveAuthorizationRole :roleId => role.roleId, :failIfUsed => opts[:force]
 end
+
+
+opts :rename do
+  summary "Rename a role"
+  arg :old, "Old name", :type => :string
+  arg :new, "New name", :type => :string
+end
+
+def rename old, new
+  role = cur_auth_mgr.roleList.find { |x| x.name == old }
+  err "no such role #{old.inspect}" unless role
+  cur_auth_mgr.UpdateAuthorizationRole :roleId => role.roleId,
+                                       :newName => new,
+                                       :privIds => role.privilege
+end
+
+
+opts :add_privilege do
+  summary "Add privileges to a role"
+  arg :name, "Role name", :type => :string
+  arg :privileges, "Privileges", :type => :string, :multi => true
+end
+
+def add_privilege name, privileges
+  role = cur_auth_mgr.roleList.find { |x| x.name == name }
+  err "no such role #{name.inspect}" unless role
+  cur_auth_mgr.UpdateAuthorizationRole :roleId => role.roleId,
+                                       :newName => role.name,
+                                       :privIds => (role.privilege | privileges)
+
+end
+
+
+opts :remove_privilege do
+  summary "Remove privileges from a role"
+  arg :name, "Role name", :type => :string
+  arg :privileges, "Privileges", :type => :string, :multi => true
+end
+
+def remove_privilege name, privileges
+  role = cur_auth_mgr.roleList.find { |x| x.name == name }
+  err "no such role #{name.inspect}" unless role
+  cur_auth_mgr.UpdateAuthorizationRole :roleId => role.roleId,
+                                       :newName => role.name,
+                                       :privIds => (role.privilege - privileges)
+
+end
